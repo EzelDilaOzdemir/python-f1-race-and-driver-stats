d9ecc4efb992ae29101f1a1ab7903ea7284e1031
EzelDilaOzdemir <ezeldilaozdemir@gmail.com>
2026-04-22 18:07:12 +0000

    wrote helper functions

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 #DSA102 COMPUTER PROGRAMMING PROJECT
+
+import csv
+import math
+import os 
+import random 
+
+#SECTION 1: HELPER FUNCTIONS 
+
+def safe_int(value, default = 0):
+    """safely converts any value to integer as the datasets use "//N" as a null
+     and has empty strings for missing cells """
+    if value is None:
+        return default
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        return default 
+    
+def safe_float(value, default = 0.0):
+    """safely converts any value to float """
+    if value is None:
+        return default
+    try:
+        return float(value)
+    except (ValueError, TypeError):
+        return default
